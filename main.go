406e71afa102fc4730ae62cdb3465f4c943b9a3f
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-02-13 02:13:23 +0600

    built the movie api with get, post, put, delete

--- main.go
+++ main.go
@@ -24,7 +24,18 @@ type Director struct {
 
 var movies []Movie
 
+
+
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies:{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", creatMovie).Methods("POST")
+	r.HandleFunc("movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080",r))
 }
