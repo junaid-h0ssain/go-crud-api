1d425aea929041f7aeb38f56e8f8278855e035c1
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-02-13 01:27:33 +0600

    defined structs

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.25.6
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc()
+}
